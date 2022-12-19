414e2b3854b6b8192b54a2e2d226384788a61066
avnerjose <avnerj.g.r@gmail.com>
2022-12-19 20:39:37 -0300

    Class 3

---
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -0,0 +1,30 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notification';
+import { Notification as PrismaNotification } from '@prisma/client';
+
+export class PrismaNotificationMapper {
+  static toPrisma(notification: Notification) {
+    return {
+      id: notification.id,
+      category: notification.category,
+      content: notification.content.value,
+      recipientId: notification.recipientId,
+      readAt: notification.readAt,
+      createdAt: notification.createdAt,
+    };
+  }
+
+  static toDomain(notification: PrismaNotification): Notification {
+    return new Notification(
+      {
+        category: notification.category,
+        content: new Content(notification.content),
+        recipientId: notification.recipientId,
+        canceledAt: notification.canceledAt,
+        createdAt: notification.createdAt,
+        readAt: notification.readAt,
+      },
+      notification.id,
+    );
+  }
+}
--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -9,4 +9,33 @@ export class InMemoryNotificationsRepository
   async create(notification: Notification) {
     this.notifications.push(notification);
   }
+
+  async save(notification: Notification): Promise<void> {
+    const notificationIndex = this.notifications.findIndex(
+      (item) => item.id === notification.id,
+    );
+
+    if (notificationIndex >= 0) {
+      this.notifications[notificationIndex] = notification;
+    }
+  }
+
+  async findById(notificationId: string): Promise<Notification | null> {
+    return (
+      this.notifications.find((item) => item.id === notificationId) ?? null
+    );
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    return this.notifications.reduce(
+      (acc, cur) => (cur.recipientId === recipientId ? acc + 1 : acc),
+      0,
+    );
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    return this.notifications.filter(
+      (item) => item.recipientId === recipientId,
+    );
+  }
 }
